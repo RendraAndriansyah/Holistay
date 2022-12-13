0656f5bd4d535d8340da3513ca83c19cab30e82c
Giyas <giyas231@gmail.com>
2022-12-14 02:22:25 +0700

    tampilan sudah sesuai kecuali modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
-    colors: {
-      'alta-dark': '#17345f',
-      'alta-light': '#f4f7fb',
-      'alta-white': '#ffffff',
-    },
-  },
-  plugins: [require('daisyui')],
+    extend: {
+      colors: {
+        'alta-dark': '#17345f',
+        'alta-light': '#f4f7fb',
+        'alta-white': '#ffffff',
+      },},},
+  plugins: [require(`daisyui`)],
 }
+
