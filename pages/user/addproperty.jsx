48f63fec5acbd708336803df3b330ba61599b6f0
travelite <rendrabootcamp@gmail.com>
2022-12-13 14:10:41 +0700

    test

--- pages/user/addproperty.jsx
+++ pages/user/addproperty.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import Swal from "sweetalert2";
 
 const AddProperty = () => {
-
   return (
     <div className="flex flex-row h-full">
-      <div className="basis 1/5 bg-red-200 w-80 "></div>
-      <div className=" bg-violet-200 w-full pt-10">
+      <div className="basis 1/5  w-80 "></div>
+      <div className=" w-full pt-10">
         <h2 className="text-3xl font-semibold text-alta-dark">
           Add New Properties
         </h2>
         <form action="">
-          <div className="flex flex-row pt-8 bg-yellow-200 ">
+          <div className="flex flex-row pt-8  ">
             <div className="flex flex-col text-xl gap-5">
               <label
                 htmlFor=""
@@ -112,7 +111,16 @@ const AddProperty = () => {
                 <button className="border-2 border-alta-dark w-24 h-8 text-alta-dark rounded-md">
                   Cancel
                 </button>
-                <button className="bg-alta-dark w-24 h-8 text-white rounded-md">
+                <button
+                  className="bg-alta-dark w-24 h-8 text-white rounded-md"
+                  onClick={Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    text: "Properties successfully added",
+                    showConfirmButton: false,
+                    timer: 2000,
+                  })}
+                >
                   Save
                 </button>
               </div>
