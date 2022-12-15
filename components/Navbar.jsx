74b94aa9afcf300800f37af0c0e3bca74d9cbf33
travelite <rendrabootcamp@gmail.com>
2022-12-15 07:35:16 +0700

    consum API pages my Profile & fixed register

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,43 +1,81 @@
-import React from 'react'
-import Image from 'next/image'
-import Router from 'next/router'
-import { useCookies } from 'react-cookie'
+import React from "react";
+import Image from "next/image";
+import Router from "next/router";
+import { useCookies } from "react-cookie";
 
 const Navbar = () => {
   const [cookie, removeCookie] = useCookies();
 
   const onLogout = () => {
-      removeCookie("name")
-      removeCookie("token")
-      Router.push({pathname:`/auth/login`})
-  }
+    Router.push({ pathname: `/auth/login` });
+    removeCookie("name");
+    removeCookie("token");
+  };
   return (
     <div className="navbar h-20 w-full bg-alta-dark px-20 py-4">
       <div className="flex-1">
-        <a className="hover:scale-110" onClick={() => Router.push({pathname:`/`})}>
-          <Image src={'/../public/logo-ALTA-v2@2x.png'} width={90} height={40} alt={'logo'}/> 
+        <a
+          className="hover:scale-110"
+          onClick={() => Router.push({ pathname: `/` })}
+        >
+          <Image
+            src={"/../public/logo-ALTA-v2@2x.png"}
+            width={90}
+            height={40}
+            alt={"logo"}
+          />
         </a>
       </div>
       <div className="flex-none gap-2">
-        <p className='text-lg font-semibold text-alta-light'>{cookie.name}</p>
+        <p className="text-lg font-semibold text-alta-light">{cookie.name}</p>
         <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar border border-alta-light">
+          <label
+            tabIndex={0}
+            className="btn btn-ghost btn-circle avatar border border-alta-light"
+          >
             <div className="w-10 rounded-full">
               {/* <img src="https://placeimg.com/80/80/people" /> */}
               <img src={cookie.image} />
             </div>
-              
           </label>
-          <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-alta-light rounded-box w-40">
-            <li><a className='text-alta-dark' onClick={() => Router.push({pathname:`/user`})}>My Profile</a></li>
-            <li><a className='text-alta-dark' onClick={() => Router.push({pathname:`/user/myproperties`})}>My Properties</a></li>
-            <li><a className='text-alta-dark' onClick={() => Router.push({pathname:`/user/historytrip`})}>History Trip</a></li>
-            <li><a className='text-alta-dark' onClick={() => onLogout()}>Logout</a></li>
+          <ul
+            tabIndex={0}
+            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-alta-light rounded-box w-40"
+          >
+            <li>
+              <a
+                className="text-alta-dark"
+                onClick={() => Router.push({ pathname: `/user` })}
+              >
+                My Profile
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-alta-dark"
+                onClick={() => Router.push({ pathname: `/user/myproperties` })}
+              >
+                My Properties
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-alta-dark"
+                onClick={() => Router.push({ pathname: `/user/historytrip` })}
+              >
+                History Trip
+              </a>
+            </li>
+            <li>
+              <a className="text-alta-dark" onClick={() => onLogout()}>
+                Logout
+              </a>
+            </li>
           </ul>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
